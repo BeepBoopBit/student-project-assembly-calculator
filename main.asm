c77140fa8dcddb983da903a560b9e456b8ee4d85
Ryoji Bernardo <ryojibernardo@gmail.com>
2022-07-15 01:24:45 +0900

    Added code in main.asm
    + prompt and I/O where added
    - feature are not yet added (such storing in stack and design)

---
+++ main.asm
@@ -0,0 +1,172 @@
+.model small
+.stack
+.data
+ 
+    first_num   db "Enter the first number: $"
+    second_num  db "Enter the second number: $"
+    ope_prompt  db "'+' Add | '-' Subtract | '*' Multiply | '/' Divide $"
+    operation   db "Enter Operation: $" 
+    ans_prompt  db "The answer is: $"
+    end_prompt  db "Continue (y/n)? $"
+    new_line    db 0ah, 0dh, '$'
+
+.code
+main proc
+    mov ax, @data
+    mov ds, ax
+    
+    ; clear Screen
+    mov ah, 06h
+    mov al, 0
+    mov bh, 7
+    mov ch, 0
+    mov cl, 0
+    mov dh, 24
+    mov dl, 79
+    int 10h
+
+MainProgram:
+    ; prompt 'firstNum' message
+    Check_first:
+        mov ah, 09h
+        mov dx, offset new_line
+        int 21h
+        mov dx, offset first_num
+        int 21h
+        
+        ; get input
+        mov ah, 01h
+        int 21h
+
+        mov ah, 09h        
+        mov dx, offset new_line
+        int 21h
+
+        ; check number if out of bound
+        CMP al, '0'
+        JB Check_first              ; uhm should have "said wrong input?" (suggest) - ryoji
+        CMP al, '9'
+        JA check_first
+
+    Check_second:
+        mov ah, 09h
+        mov dx, offset second_num
+        int 21h
+
+        ; get input
+        mov ah, 01h
+        int 21h
+
+        mov ah, 09h 
+        mov dx, offset new_line
+        int 21h
+
+        ; check number if out of bound
+        CMP al, '0'
+        JB Check_second
+        CMP al, '9'
+        JA Check_second
+
+        mov ah, 09h 
+        mov dx, offset new_line
+        int 21h
+
+    Check_operation:
+        mov ah, 09h
+        mov dx, offset ope_prompt
+        int 21h
+        mov dx, offset new_line
+        int 21h
+        mov dx, offset operation
+        int 21h
+
+        ; get input
+        mov ah, 01h
+        int 21h
+
+        mov ah, 09h 
+        mov dx, offset new_line
+        int 21h
+
+        ; check if basic operations
+        CMP al, '+'
+        JE Addition
+        CMP al, '-'
+        JE Subtraction
+        CMP al, '/'
+        JE Division
+        CMP al, '*'
+        JE Multiplication    
+        JMP Check_operation
+
+    Main_relative:
+        JMP MainProgram
+
+    Answer:
+        mov ah, 09h 
+        mov dx, offset new_line
+        int 21h
+
+        mov ah, 09h 
+        mov dx, offset ans_prompt
+        int 21h
+
+        mov ah, 09h 
+        mov dx, offset new_line
+        int 21h
+
+    Continue:
+        mov ah, 09h 
+        mov dx, offset end_prompt
+        int 21h
+        
+        ; get input
+        mov ah, 01h
+        int 21h
+
+        mov ah, 09h 
+        mov dx, offset new_line
+        int 21h
+        
+        ; check if (y/n)
+        CMP al, 'y'
+        JE Main_relative
+        CMP al, 'n'
+        JE Exit
+
+        JMP Continue
+        
+        Exit:
+            mov ah, 4ch
+            int 21h
+
+
+    ; basic operation function
+    Addition:
+        
+        ; your code here
+        
+        JMP Answer
+
+    Subtraction:
+
+        ; your code here
+
+        JMP Answer
+
+    Division:
+
+        ; your code here
+
+        JMP Answer
+
+    Multiplication:
+
+        ; your code here
+
+        JMP Answer
+
+main endp
+end main
+
+C:\Users\Ryoji\Desktop\assembly-calculator
